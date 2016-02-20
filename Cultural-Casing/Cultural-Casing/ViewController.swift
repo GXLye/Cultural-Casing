ecfbfd1aee7806837fb5cb5b6a2c7289e1e0572c
Lye Guang Xing <gxlye@me.com>
2016-02-20 23:10:37 +0800

    Added KLCC models
    - Added KLCC Top and bottom model
    - Initial commit of the iOS app

---
+++ Cultural-Casing/Cultural-Casing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cultural-Casing
+//
+//  Created by GXLye on 20/02/2016.
+//  Copyright © 2016 Sweatshop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
